ec2b848c510210e861db60bf0a84ac579704d2c1
gzuppa <gustavo_zuppa_flores@hotmail.com>
2019-01-03 12:52:43 -0600

    yamecansedeescribir

--- front/src/components/Arduino/ArduinoLed.js
+++ front/src/components/Arduino/ArduinoLed.js
@@ -8,6 +8,10 @@ import Protoboard from '../../images/proto.png';
 import OneIcon from '../../images/numbers/one.png'
 import TwoIcon from '../../images/numbers/two.png'
 import Firmata from '../../images/ejemplos/sketchfirmata.png'
+import ThreeIcon from '../../images/numbers/three.png'
+import LedRojo from '../../images/ejemplos/arduinoledrojo.png'
+import FourIcon from '../../images/numbers/four.png'
+import FiveIcon from '../../images/numbers/five.png'
 
 class ArduinoLed extends Component{
 
@@ -124,6 +128,85 @@ class ArduinoLed extends Component{
         Una vez que hayas seleccionado el sketch, presiona el botón de <strong>Subir</strong> que se encuentra en los menús principales. Inmediatamente
         se desplegará en la parte inferior de tu IDE el avance del procedimiento. Una vez que llega al 100% tu sketch está cargado en la placa y listo para
         usarse.
+      </p>
+    </div>
+  </div>
+</article>
+
+<article class="media section-subtitle">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src={ThreeIcon}/>
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Conectando el Arduino</strong> <small>Conociendo los componentes electrónicos y la forma de conectar</small> 
+        <br/>
+        Tu placa Arduino tiene 16 pines digitales ennumerados en el borde de la misma. Cada uno de ellos tiene un objetivo particular, sin embargo
+        los 16 son capaces de transmitir energía y ciertas órdenes. Para nuestro caso pŕáctico de hoy vamos a utilizar un LED sencillo, el cual deberás
+        conectar con la patilla larga en el <strong>PIN GND</strong> y la patilla corta en el <strong>PIN 13</strong>
+         <p><img class="exampleimages" src={LedRojo}/></p>
+      </p>
+    </div>
+  </div>
+</article>
+
+<article class="media section-subtitle">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src={FourIcon}/>
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Johnny Five!</strong> <small>¿Que es un Framework y para que me servirá?</small> 
+        <br/>
+        Un Framework es una herramienta de terceros que te permite realizar integraciones o funciones no nativas en ciertos lenguajes o entornos de
+        programación. Para nuestro caso, el Arduino tiene su propio lenguaje que utilizamos en el IDE, sin embargo, para que podamos programarlo via
+        Web, usaremos además del protocolo Firmata, el Framework <a href="http://johnny-five.io/"><strong>Johnny-Five</strong></a> el cual está escrito en JavaScript.
+        Este Framework nos permitirá una integración más sencilla con nuestras aplicaciones. 
+
+      </p>
+    </div>
+  </div>
+</article>
+
+<article class="media section-subtitle">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src={FiveIcon}/>
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Montando un pequeño servidor</strong> <small>Paso a paso para tener rapidamente un servidor funcional</small> 
+        <br/>
+        Vamos a montar un servidor en nuestro equipo para utilizarlo de manera local. Cabe señalar, que practicaremos en entornos locales pero todo lo
+        que tu realices puede ser publicado en la Web cuando quieras, para ello, dedicaremos algunos otros temas posteriores.
+        <br/>
+        Primero, debemos instalar <a href="https://nodejs.org/es/"><strong>NodeJS</strong></a> descargando de su página oficial. Los pasos para la instalación
+        son muy sencillos. Para <strong>Windows</strong> solamente descomprime el archivo y abre el ejecutable, sigue los pasos y tendrás Node en tu equipo.
+        Si utilizas Linux <strong>abre una terminal</strong> y ejecuta los siguientes comandos:
+         
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+              <p>      
+          <strong> sudo apt-get update </strong><br/>
+          <strong> sudo apt-get install nodejs </strong>
+              </p>
+        </div>
+        </div>
+           </article>
+        </div>
+
+      Ahora vamos a utilizar <strong>NPM</strong> que es un manejador de paquetes perteneciente a NodeJS
+
       </p>
     </div>
   </div>
