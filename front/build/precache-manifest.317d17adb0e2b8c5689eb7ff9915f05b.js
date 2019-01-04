bf852da9916bf1f7290418d569cf550ba664de9c
gzuppa <gustavo_zuppa_flores@hotmail.com>
2019-01-03 22:42:08 -0600

    ya esta el build

---
+++ front/build/precache-manifest.317d17adb0e2b8c5689eb7ff9915f05b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "348746763a36a2cef7f292230ad5fac0",
+    "url": "/static/media/arduinomotor.34874676.png"
+  },
+  {
+    "revision": "046fb5beaa48fe3f9033",
+    "url": "/static/css/main.93a46f38.chunk.css"
+  },
+  {
+    "revision": "5a73d545022a95b0699b",
+    "url": "/static/js/1.5a73d545.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c1f396dd88922712c8b113b95c5ea3e",
+    "url": "/static/media/bender.6c1f396d.png"
+  },
+  {
+    "revision": "b3539526da1803b710b4e4f51d32fc74",
+    "url": "/static/media/arduinokit.b3539526.png"
+  },
+  {
+    "revision": "01e9ec9e0242dd76f78eb167795509be",
+    "url": "/static/media/codekids.01e9ec9e.png"
+  },
+  {
+    "revision": "36c8e8231a16ae79a714d1b8a1a31c7f",
+    "url": "/static/media/banner-img.36c8e823.png"
+  },
+  {
+    "revision": "b0e4b9e495850e2ca83d17fbd63d3cb5",
+    "url": "/static/media/teach.b0e4b9e4.jpg"
+  },
+  {
+    "revision": "2b7f4b2c80b6880d15147f0362d48434",
+    "url": "/static/media/shopp.2b7f4b2c.jpg"
+  },
+  {
+    "revision": "63ea39f88b567c2f2e49c8c1966b577d",
+    "url": "/static/media/aboutt.63ea39f8.jpg"
+  },
+  {
+    "revision": "e3b2e894563c03d0aed2cc2dba2f8c64",
+    "url": "/static/media/vintage.e3b2e894.jpg"
+  },
+  {
+    "revision": "12ae26a70df86c047e2e86f46db0434d",
+    "url": "/static/media/spiderman.12ae26a7.png"
+  },
+  {
+    "revision": "558eed9b727933f94dc907213ba2f97f",
+    "url": "/static/media/zombieavatar.558eed9b.png"
+  },
+  {
+    "revision": "b7eda8e6f00377b6a79a01e59513b4d2",
+    "url": "/static/media/mario.b7eda8e6.png"
+  },
+  {
+    "revision": "e5b0dc4fdf2f82d2fdd1bd8bd71130e6",
+    "url": "/static/media/ryu.e5b0dc4f.png"
+  },
+  {
+    "revision": "eb040303b7d170466e646e91ed222fb5",
+    "url": "/static/media/link.eb040303.png"
+  },
+  {
+    "revision": "1e973630fd9bdfaa0be87fe3b40eda89",
+    "url": "/static/media/re1.1e973630.jpg"
+  },
+  {
+    "revision": "5a9753916499e82f36c76cb56e9c1a78",
+    "url": "/static/media/space.5a975391.png"
+  },
+  {
+    "revision": "9f834e13927356f8c95b235afb0a898c",
+    "url": "/static/media/mk.9f834e13.png"
+  },
+  {
+    "revision": "c6d21c1b051320d2e48d5cbbc1150865",
+    "url": "/static/media/vintagerobot.c6d21c1b.jpg"
+  },
+  {
+    "revision": "08d0bb052abd768c929aea857744c5d4",
+    "url": "/static/media/arduinoled.08d0bb05.png"
+  },
+  {
+    "revision": "7d7b6e99f40c7e55f2e9c6175c6db5b5",
+    "url": "/static/media/arduinolcd.7d7b6e99.png"
+  },
+  {
+    "revision": "046fb5beaa48fe3f9033",
+    "url": "/static/js/main.046fb5be.chunk.js"
+  },
+  {
+    "revision": "dd8eab46234ad790f396b02c4863ab18",
+    "url": "/static/media/arduinobutton.dd8eab46.png"
+  },
+  {
+    "revision": "04f809224ebe0dd6701dce7413f698c0",
+    "url": "/static/media/leds.04f80922.png"
+  },
+  {
+    "revision": "4edd06515e048e40996199e57b0fc88d",
+    "url": "/static/media/proto.4edd0651.png"
+  },
+  {
+    "revision": "adf6e1c96062f40145a8d01f32aa9ef3",
+    "url": "/static/media/two.adf6e1c9.png"
+  },
+  {
+    "revision": "199662fa8bd2706fd4ab2095b1bf9168",
+    "url": "/static/media/sketchfirmata.199662fa.png"
+  },
+  {
+    "revision": "660a425b5f4109bdfd50847e78a104a9",
+    "url": "/static/media/three.660a425b.png"
+  },
+  {
+    "revision": "e86a33926466573649e64719c0b33969",
+    "url": "/static/media/arduinoledrojo.e86a3392.png"
+  },
+  {
+    "revision": "d3377365ef4e371c861e7aa419150c7f",
+    "url": "/static/media/four.d3377365.png"
+  },
+  {
+    "revision": "4ff581ca99929d4813c580ff6a267ec5",
+    "url": "/static/media/five.4ff581ca.png"
+  },
+  {
+    "revision": "49f3f91e5badaa65cc03aa7023d3bb0a",
+    "url": "/static/media/six.49f3f91e.png"
+  },
+  {
+    "revision": "d48638befbc5d554ce5c68425dd52fb2",
+    "url": "/static/media/codeledarduino.d48638be.png"
+  },
+  {
+    "revision": "7b4647b82a85a1ea369be796d208ccd0",
+    "url": "/static/media/bonus.7b4647b8.png"
+  },
+  {
+    "revision": "264710a4e7826b35df2e562895c44ee2",
+    "url": "/static/media/pika.264710a4.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8bb77fce3a4d9dc233300aef10e362f1",
+    "url": "/static/media/whiterobot.8bb77fce.jpg"
+  },
+  {
+    "revision": "b3488e8486d2b4e3a0666997f91c7ed9",
+    "url": "/static/media/Ubuntu-Regular.b3488e84.ttf"
+  },
+  {
+    "revision": "d0fc30856a541539009e73c787a68bc1",
+    "url": "/static/media/Ubuntu-Bold.d0fc3085.ttf"
+  },
+  {
+    "revision": "294108f0de477c61aa4cbc752d744da1",
+    "url": "/index.html"
+  }
+];
